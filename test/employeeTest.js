631a1f55e9fd87f47910ca31fc943efdd59b991f
DolngNothing <1178363215@qq.com>
2020-08-27 18:46:58 +0800

    [employee] [christopher] feature: add test validateType throw

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -5,3 +5,12 @@ employeeTest('test toString',t => {
     const employee =new Employee("jack","engineer");
     t.is("jack (engineer)",employee.toString())
 })
+
+employeeTest('test validateType throw',t => {
+    try{
+        new Employee("jack","6666");
+        t.fail();
+    }catch(e){
+        t.is('Employee cannot be of type 6666',e.message)
+    }
+})
