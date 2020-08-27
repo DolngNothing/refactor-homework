a3966e4b2e8a7b2f07f3222ed1a8eb2660e24a2c
DolngNothing <1178363215@qq.com>
2020-08-27 17:58:55 +0800

    [delivery] [christopher] refactor: extract notrushDelivery

--- src/delivery.js
+++ src/delivery.js
@@ -1,26 +1,9 @@
 function deliveryDate (anOrder, isRush) {
-  let deliveryTime;
   if (isRush) {
     return rushDelivery(anOrder)
   }
   else {
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+    return notRushDelivery(anOrder)
   }
 }
 
@@ -42,6 +25,27 @@ rushDelivery=(anOrder)=>{
     deliveryTime = 3;
   }
   return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+notRushDelivery=(anOrder)=>{
+  let deliveryTime=0;
+  if ([
+    'MA',
+    'CT',
+    'NY',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else if ([
+    'ME',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  }
+  else {
+    deliveryTime = 4;
+  }
+  return anOrder.placedOn.plusDays(2 + deliveryTime);
 }
 
 
