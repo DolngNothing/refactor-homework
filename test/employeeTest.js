bd6361d15a2cbc66a64b28e1f20e406764a52b0b
DolngNothing <1178363215@qq.com>
2020-08-27 18:44:52 +0800

    [employee] [christopher] feature: add test toString

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -1,7 +1,7 @@
 const { Employee } = require('../src/Employee');
 const employeeTest = require('ava');
 
-employeeTest('toString',t => {
+employeeTest('test toString',t => {
     const employee =new Employee("jack","engineer");
     t.is("jack (engineer)",employee.toString())
 })
