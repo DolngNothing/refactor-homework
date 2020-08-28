43b76aa0a3bb07120033dee703f6adec86d67681
DolngNothing <1178363215@qq.com>
2020-08-28 17:25:35 +0800

    [delivery] [christopher] refactor: simpify deliveryDate

--- src/delivery.js
+++ src/delivery.js
@@ -1,24 +1,18 @@
 function deliveryDate (anOrder, isRush) {
-  if (isRush) {
-    return rushDelivery(anOrder)
-  }
-  else {
-    return notRushDelivery(anOrder)
-  }
+  return isRush? rushDelivery(anOrder):notRushDelivery(anOrder)
 }
 
 rushDelivery=(anOrder)=>{
-  const plusDay=1;
   const nearCity = ['MA','CT',];
   const farCity = ['NY','NH',];
   if (nearCity.includes(anOrder.deliveryState)) {
-    return anOrder.placedOn.plusDays(plusDay + 1);
+    return anOrder.placedOn.plusDays(2);
   }
   else if (farCity.includes(anOrder.deliveryState)) {
-    return anOrder.placedOn.plusDays(plusDay + 2);
+    return anOrder.placedOn.plusDays(3);
   }
   else {
-    return anOrder.placedOn.plusDays(plusDay + 3);
+    return anOrder.placedOn.plusDays(4);
   }
 }
 
