679802d0b226adb540f4d8f943160d5a0372eb1d
DolngNothing <1178363215@qq.com>
2020-08-28 20:45:09 +0800

    [print] [christopher] refactor: extract calculateOutstanding

--- src/print.js
+++ src/print.js
@@ -1,15 +1,19 @@
 function printOwing(invoice) {
-  let outstanding = 0;
   let result = '***********************\n**** Customer Owes ****\n***********************\n';
-  // calculate outstanding
-  for (const o of invoice.borderSpacing) {
-    outstanding += o.amount;
-  }
-  result += `name: ${invoice.customer}\namount: ${outstanding}\ndate: ${createDueDate(new Date()).toLocaleDateString()}`;
+  result += `name: ${invoice.customer}\namount: ${calculateOutstanding(invoice.borderSpacing)}\ndate: ${createDueDate(new Date()).toLocaleDateString()}`;
   return result;
 }
+
 createDueDate = (today) => {
   return new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+calculateOutstanding=(borderSpacing)=>{
+  let outstanding=0;
+  for (const o of borderSpacing) {
+    outstanding += o.amount;
+  }
+  return outstanding;
 }
 
 module.exports = { printOwing }
